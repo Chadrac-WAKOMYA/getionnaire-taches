a9bedbf72e283ca7353aec1f0c153eb1afa9765a
KIGHUNDILA WAKOMYA Chadrac <chadracxx@gmail.com>
2024-12-27 12:29:03 +0200

    Blog layout modified

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,11 +1,7 @@
-export default function RootLayout({
+export default function BlogLayout({
     children,
   }: {
     children: React.ReactNode
   }) {
-    return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    )
+    return <section>{children}</section>
   }
