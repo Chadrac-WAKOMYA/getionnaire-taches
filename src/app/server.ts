5c9d123ac11d9af8ce2bcefe7645f8f45c767c69
KIGHUNDILA WAKOMYA Chadrac <chadracxx@gmail.com>
2024-12-27 17:35:23 +0200

    beginning with the server

---
+++ src/app/server.ts
@@ -0,0 +1,13 @@
+// server.ts
+import express from 'express';
+
+const app = express();
+const PORT = 5000;
+
+app.get('/api/taches', (req, res) => {
+  res.json([{ id: 1, titre: 'Tâche 1' }]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
+});
