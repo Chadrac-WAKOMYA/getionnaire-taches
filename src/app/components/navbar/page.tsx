3bb0546ab7add479dcd5dfa7a73aaa457ab76474
KIGHUNDILA WAKOMYA Chadrac <chadracxx@gmail.com>
2024-12-27 15:02:45 +0200

    dzhboard in navigation

--- src/app/components/navbar/page.tsx
+++ src/app/components/navbar/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const Navbar : React.FC = () => {
     return (
         <nav>
-            <Link href = "/">Acceuil</Link>
+            <Link href = "/dashboard">Acceuil</Link>
             <Link href = "/about">A propos</Link>
         </nav>
     );
