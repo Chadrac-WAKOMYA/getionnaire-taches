0beaeef146dda566bfecf90f0a67ec274bfbf7fd
KIGHUNDILA WAKOMYA Chadrac <chadracxx@gmail.com>
2024-12-28 10:03:14 +0200

    first post endpoints added

--- src/app/server.ts
+++ src/app/server.ts
@@ -4,8 +4,16 @@ import express from 'express';
 const app = express();
 const PORT = 5000;
 
+//Recupération de tâches
 app.get('/api/taches', (req, res) => {
   res.json([{ id: 1, titre: 'Tâche 1' }, { id: 2, titre: 'Tâche 2' }]);
+});
+
+//Ajout tache
+app.post('/api/taches', (req, res) => {
+    const nouvelleTache = req.body; // Assurez-vous d'utiliser body-parser
+    // Ajoutez la logique pour sauvegarder la tâche
+    res.status(201).json(nouvelleTache);
 });
 
 app.listen(PORT, () => {
